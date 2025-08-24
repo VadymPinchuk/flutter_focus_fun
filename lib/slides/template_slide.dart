a08321df05159014ec87c930ac51c81755bb0918
Vadym Pinchuk <vadym.pinchuk@sky.uk>
2025-08-24 22:09:35 +0100

    template changed a bit

--- lib/slides/template_slide.dart
+++ lib/slides/template_slide.dart
@@ -6,12 +6,12 @@ import 'package:flutter_focus_fun_tv_demo/slides/widgets/footer_widget.dart';
 /// A private class to hold the text styles for the slide.
 class _SlideTextStyles {
   static const TextStyle titleStyle = TextStyle(
-    fontSize: 72,
+    fontSize: 64.0,
     fontWeight: FontWeight.bold,
     color: AppColors.contentTitle,
   );
   static const TextStyle subtitleStyle = TextStyle(
-    fontSize: 56,
+    fontSize: 52.0,
     color: AppColors.contentSubtitle,
   );
 }
@@ -34,38 +34,23 @@ class TemplateSlide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isHorizontalNavBar = context.settingsModel.experience.value.isMobile;
-    final railPadding = isHorizontalNavBar ? 48.0 : 80.0;
-
-    return Stack(
-      children: [
-        Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: railPadding,
-            vertical: 48.0,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(title, style: _SlideTextStyles.titleStyle),
-              const SizedBox(height: 8.0),
-              Text(subtitle, style: _SlideTextStyles.subtitleStyle),
-              const SizedBox(height: 32.0),
-              // Constrain the height of the body content.
-              child,
-            ],
-          ),
-        ),
-        // Position the static footer at the bottom right.
-        Container(
-          alignment: Alignment.bottomRight,
-          padding: EdgeInsets.symmetric(
-            horizontal: railPadding,
-            vertical: context.railData.tileSize.height / 2,
-          ),
-          child: const FooterWidget(),
-        ),
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: context.railData.railHorizontalPadding,
+        vertical: context.railData.tileSize.height / 2,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title, style: _SlideTextStyles.titleStyle),
+          const SizedBox(height: 8.0),
+          Text(subtitle, style: _SlideTextStyles.subtitleStyle),
+          const SizedBox(height: 32.0),
+          // Constrain the height of the body content.
+          Expanded(child: child),
+          Align(alignment: Alignment.bottomRight, child: const FooterWidget()),
+        ],
+      ),
     );
   }
 }
